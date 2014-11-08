9ec2c0cfdb23ef3a4d7f75a2c1a4130c4df9c39f
Dmitry <daoh@yandex.ru>
2014-11-08 23:47:37 +0300

    convert

--- convertor.py
+++ convertor.py
@@ -6,7 +6,8 @@ class Convertor():
         pass
 
     def cite_book_to_book(self, template):
-        dic = {"first": "", "last": "", "title": "", "url": "", "year": "", "isbn": "", "location": "", "publisher": "", "volume": ""}
+        dic = {"first": "", "last": "", "title": "", "url": "", "year": "", "isbn": "", "location": "",
+               "publisher": "", "volume": "", "encyclopedia": "", "editor1-first": "", "editor1-last": ""}
         for key in dic:
             pattern = re.compile("\|[\s]*"+key+"[\s]*=")
             if pattern.search(template) is not None:
@@ -15,6 +16,12 @@ class Convertor():
                     dic[key] = tmp[:tmp.find("|")]
                 elif tmp.find("}") != -1:
                     dic[key] = tmp[:tmp.find("}")]
+        if dic["title"] == "":
+            dic["title"] = dic["encyclopedia"]
+        if dic["first"] == "":
+            dic["firdt"] = dic["editor1-first"]
+        if dic["last"] == "":
+            dic["last"] = dic["editor1-last"]
         res = "* {{книга\n"
         res += " | автор         = " + dic["first"] + " " + dic["last"] + "\n"
         res += " | заглавие      = " + dic["title"] + "\n"
@@ -31,6 +38,14 @@ class Convertor():
     def cite_journal_to_article(self, template):
         dic = {"first": "", "last": "", "journal": "", "title": "", "url": "", "year": "", "number": "",
                "doi": "", "location": "", "publisher": "", "volume": "", "pages": "", "date": "", "issn": "", "isbn": ""}
+        for key in dic:
+            pattern = re.compile("\|[\s]*"+key+"[\s]*=")
+            if pattern.search(template) is not None:
+                tmp = template[pattern.search(template).end():]
+                if tmp.find("|") != -1:
+                    dic[key] = tmp[:tmp.find("|")]
+                elif tmp.find("}") != -1:
+                    dic[key] = tmp[:tmp.find("}")]
         year = dic["year"]
         if dic["year"] == "":
             year = dic["date"]
@@ -41,12 +56,13 @@ class Convertor():
         res += " |язык          = " + "en" + "\n"
         res += " |издание       = " + dic["journal"] + "\n"
         res += " |год           = " + year + "\n"
-        res += " |том           = " +  + "\n"
-        res += " |номер         = " +  + "\n"
-        res += " |страницы      = " +  + "\n"
-        res += " |doi           = " +  + "\n"
-        res += " |isbn          = " +  + "\n"
-        res += " |issn          = " + dic["last"] + "\n"
+        res += " |том           = " + dic["volume"] + "\n"
+        res += " |номер         = " + dic["number"] + "\n"
+        res += " |страницы      = " + dic["pages"] + "\n"
+        res += " |doi           = " + dic["doi"] + "\n"
+        res += " |isbn          = " + dic["isbn"] + "\n"
+        res += " |issn          = " + dic["issn"] + "\n"
+        res += " | ref          = " + dic["last"] + "\n"
         res += "}}"
         return res
 
@@ -57,11 +73,13 @@ class Convertor():
             unit = unit.strip()
             pattern = re.compile("\{\{[\s]*cite[\s]*book[\s]*\|")
             if pattern.search(unit) is not None:
-                continue
                 res = self.cite_book_to_book(unit)
                 print(res)
             pattern = re.compile("\{\{[\s]*cite[\s]*journal[\s]*\|")
             if pattern.search(unit) is not None:
-                print(unit)
                 res = self.cite_journal_to_article(unit)
-                #print(res)
+                print(res)
+            pattern = re.compile("\{\{[\s]*cite[\s]*encyclopedia[\s]*\|")
+            if pattern.search(unit) is not None:
+                res = self.cite_book_to_book(unit)
+                print(res)
